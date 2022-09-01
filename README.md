ed674ddb8bf9594314bea4d277e240b6f105546f
cesarMurillo1 <82464718+cesarMurillo1@users.noreply.github.com>
2022-09-01 18:14:50 -0500

    electiva
    electiva

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# electiva-software
+cambiar en senttings cambiar donde estan los archivos css a la direccion de su pc
+crear la base de datos
+cambiar la configuracion de la base de datos en el archivo settings
+utilizar los comandos python manage.py migracion y makemigracion 
+y listo
---
+++ electiva/proyect/GestionDedocumentos/views.py
@@ -0,0 +1,30 @@
+
+from django.shortcuts import render,redirect,get_object_or_404
+from .forms import *
+from .models import *
+from django.contrib import messages
+from django.views.generic import TemplateView
+from django.contrib.auth.models import User
+
+class Inicio(TemplateView):
+    template_name='index.html'
+
+def feed(request):
+    return render(request, 'index.html')
+
+def register(request):
+    if request.method=='POST':
+        form=userRegisterform(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data['username']
+            messages.success(request,f'Usuario{username} creado')
+            return redirect('index')
+    else:
+        form=userRegisterform()
+    context={'form':form}
+    return render(request, 'gestion/register.html',context)
+
+def profile(request):
+    current_user = get_object_or_404(User, pk=request.user.pk)
+    return render(request, 'gestion/profile.html',{'usuario':current_user})
