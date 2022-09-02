acd1348afa25762cee4e0dddd3c0538fa7642edb
Majo <ospinamj707@gmail.com>
2022-09-02 08:38:25 -0500

    1.0

---
+++ electiva/proyect/GestionDedocumentos/urls.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import views
+from django.urls import path
+from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView,LogoutView
+urlpatterns=[
+    path('',feed,name='index'),
+	path('register/', register, name='register'),
+	path('login/', LoginView.as_view(template_name='gestion/login.html'), name='login'),
+	path('logout/', LogoutView.as_view(template_name='gestion/logout.html'), name='logout'),
+    path('profile/',profile,name='profile'),
+]
---
+++ gestionDocumental/gestionD/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+
+class administrador(models.Model):
+    codigoAd = models.
